f7adafa72d213f60f95e9c08b080cc7ce5100004
GWayShh <142102069+GWayShh@users.noreply.github.com>
2024-02-25 00:35:03 +0530

    Update countdown-custom.js

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2023, 3, 21, 0)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2024, 5, 12, 0)}); // year, month, date, hour
         });
 });		
 
